0cde547a554bcfcfca77226e8b2c5902cddbecb5
maheenqamar <maheenqamarawan@gmail.com>
2024-02-29 20:49:19 +0500

    Initialize data_structure.py with class DataProcessor

---
+++ data_structure.py
@@ -0,0 +1,12 @@
+class DataProcessor:
+    def __init__(self):
+        pass
+    def process_data(self, data):
+          pass
+    def visualize_data(self, data):
+          pass
+if __name__ == "__main__":
+    data_processor = DataProcessor()
+    data = [1,2,3,4,5]
+    processed_data = data_processor.process_data(data)
+    data_processor.visualize_data(processed_data)
